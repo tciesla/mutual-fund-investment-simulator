e72029cca8100d06d726bd536eff8a7abef8142e
Tomasz Cieśla <ciesla.tomasz.92@gmail.com>
2017-04-23 23:46:46 +0200

    extracted setUp method in unit test

--- server/src/test/java/pl/tciesla/simulator/server/valuation/MutualFundsValuationCronTest.java
+++ server/src/test/java/pl/tciesla/simulator/server/valuation/MutualFundsValuationCronTest.java
@@ -1,6 +1,7 @@
 package pl.tciesla.simulator.server.valuation;
 
 import org.assertj.core.util.Lists;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -27,18 +28,22 @@ public class MutualFundsValuationCronTest {
     @InjectMocks
     private MutualFundsValuationCron mutualFundsValuationCron;
 
-    @Test
-    public void shouldUpdatedValuationBeDifferent() throws Exception {
-        // given
-        BigDecimal mutualFund1OldValuation = new BigDecimal("250.00");
-        MutualFund mutualFund1 = createMutualFund(1L, "Best of US Stocks", mutualFund1OldValuation);
+    private MutualFund mutualFund1;
+    private MutualFund mutualFund2;
 
-        BigDecimal mutualFund2OldValuation = new BigDecimal("130.00");
-        MutualFund mutualFund2 = createMutualFund(2L, "Best of UK Stocks", mutualFund2OldValuation);
-
+    @Before
+    public void setUp() throws Exception {
+        mutualFund1 = createMutualFund(1L, "Best of US Stocks", BigDecimal.valueOf(100.11));
+        mutualFund2 = createMutualFund(2L, "Best of UK Stocks", BigDecimal.valueOf(200.22));
         List<MutualFund> mutualFunds = Lists.newArrayList(mutualFund1, mutualFund2);
         doReturn(mutualFunds).when(mutualFundDao).fetchAll();
+    }
 
+    @Test
+    public void shouldUpdateMutualFundValuations() throws Exception {
+        // given
+        BigDecimal mutualFund1OldValuation = mutualFund1.getValuation();
+        BigDecimal mutualFund2OldValuation = mutualFund2.getValuation();
         // when
         mutualFundsValuationCron.updateMutualFundsValuation();
         // then
@@ -47,12 +52,7 @@ public class MutualFundsValuationCronTest {
     }
 
     @Test
-    public void shouldPersistUpdatedValuation() throws Exception {
-        // given
-        MutualFund mutualFund1 = createMutualFund(1L, "Best of US Stocks", new BigDecimal("250.00"));
-        MutualFund mutualFund2 = createMutualFund(2L, "Best of UK Stocks", new BigDecimal("130.00"));
-        List<MutualFund> mutualFunds = Lists.newArrayList(mutualFund1, mutualFund2);
-        doReturn(mutualFunds).when(mutualFundDao).fetchAll();
+    public void shouldPersistMutualFundsWithUpdatedValuation() throws Exception {
         // when
         mutualFundsValuationCron.updateMutualFundsValuation();
         // then
